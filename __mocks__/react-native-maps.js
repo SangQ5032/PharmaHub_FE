ef75ac336200f61657850e0a779a82a1414d57a6
Nguyentung7193 <nguyentung260310@gmail.com>
2025-11-24 15:52:22 +0700

    config(react-native-maps): mock react native maps

---
+++ __mocks__/react-native-maps.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MockMapView = (props) => {
+  return <></>;
+};
+
+const MockMarker = (props) => {
+  return <></>;
+};
+
+export default MockMapView;
+export const Marker = MockMarker;
+export const PROVIDER_GOOGLE = 'google';
--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     'reactotron-react-native': '<rootDir>/__mocks__/reactotron-react-native.ts',
     'react-native-gesture-handler': '<rootDir>/__mocks__/react-native-gesture-handler.js',
     '@react-native-community/geolocation': '<rootDir>/__mocks__/geolocation.js',
+    'react-native-maps': '<rootDir>/__mocks__/react-native-maps.js'
   },
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?@?react-navigation|@react-native|react-native|react-native-.*|@react-native-community/.*)"
