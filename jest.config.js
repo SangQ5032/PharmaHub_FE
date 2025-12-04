86d5a3de5436f8fe289f2e16fac2bcad6aacaff0
Nguyentung7193 <nguyentung260310@gmail.com>
2025-12-04 20:40:16 +0700

    fix(husky): mock react native vision camera

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,8 @@ module.exports = {
     'react-native-gesture-handler': '<rootDir>/__mocks__/react-native-gesture-handler.js',
     '@react-native-community/geolocation': '<rootDir>/__mocks__/geolocation.js',
     'react-native-maps': '<rootDir>/__mocks__/react-native-maps.js',
-    'react-native-qrcode-svg': '<rootDir>/__mocks__/react-native-qrcode-svg.js'
+    'react-native-qrcode-svg': '<rootDir>/__mocks__/react-native-qrcode-svg.js',
+    '^react-native-vision-camera$': '<rootDir>/__mocks__/react-native-vision-camera.js'
   },
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?@?react-navigation|@react-native|react-native|react-native-.*|@react-native-community/.*)"
